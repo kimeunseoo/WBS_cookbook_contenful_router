423764bd23ed54cf96b04ec1a8ca40f1fbe1ab9e
Fennec92 <nazar.hamidulla@posteo.de>
2022-07-15 15:58:23 +0200

    added keys

--- src/components/VS_element.js
+++ src/components/VS_element.js
@@ -9,16 +9,16 @@ const VS_element = ({foodState}) => {
       {
         foodState
         .filter(item => item.fields.name === foodParam.foodId)
-        .map(item => (
-          <div>
+        .map((item, index) => (
+          <div key={index}>
             <h2>{item.fields.name}</h2>
             <img src={item.fields.picture.fields.file.url} alt="vs" />
             <p>{item.fields.description}</p>
             <ul>
               {
                 Object.keys(item.fields.ingredients)
-                .map(ingredient => (
-                  <li>{ingredient}: {item.fields.ingredients[ingredient]}</li>
+                .map((ingredient, index) => (
+                  <li key={index}>{ingredient}: {item.fields.ingredients[ingredient]}</li>
                 ))
               }
             </ul>
